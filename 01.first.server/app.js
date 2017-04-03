01a33d99dddbc36835ad1c75a90ff12abcf01bfe
toxichl <472590061@qq.com>
2017-04-03 14:44:42 +0800

    Simple Update

--- 01.first.server/app.js
+++ 01.first.server/app.js
@@ -4,16 +4,16 @@ var http = require('http')
 var express = require('express')
 var app = express()
 
-// var server = http.createServer(function (req, res) {
-// 	console.log('Hello Server')
-// })
+var server = http.createServer(function (req, res) {
+	console.log('Hello Server')
+})
 //
 // server.listen(9000)
 
 // 将app对象注册在原生的http模块中
 http.createServer(app)
 
-	app.get('/api', function (req,res) {
+app.get('/api', function (req, res) {
 	res.send('Hello Server');
 	res.end();
 })
--- README.md
+++ README.md
@@ -1,3 +1,26 @@
-# Express 学习记录
+# express-demo
+Express的学习记录
 
-## 
+## express是什么？
+
+基于 `Node.js` 平台，快速、开放、极简的 `web` 开发框架。
+
+## 创建一个服务器
+
+回顾一下原生的创建服务器的方式：
+```js
+var server = http.createServer(function (req, res) {
+	console.log('Hello Server')
+})
+```
+再来看看express如果如何快速创建一个服务器:
+```js
+http.createServer(app)
+
+app.get('/api', function (req, res) {
+	res.send('Hello Server');
+	res.end();
+})
+
+app.listen(9000)
+```
